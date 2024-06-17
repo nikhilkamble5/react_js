e6f441a8caaba5d138cd80c53fe0c4ed60319a06
nikhil kamble <nikhilkamble19985@gmail.com>
2024-06-17 21:10:15 +0530

    3 component create other alll electonic

--- React_Day_7_Code_Categorywise_Products_App_Updated/myapp/src/App.jsx
+++ React_Day_7_Code_Categorywise_Products_App_Updated/myapp/src/App.jsx
@@ -1,13 +1,10 @@
-import React, { Component } from 'react'
-import Products from './Products'
+import React, { Component } from "react";
+import Products from "./Products";
 
 export default class App extends Component {
-
-
   render() {
-
     //product array objetc
-    const Product_Data = [
+    const Elect_Prod = [
       {
         id: 1,
         name: "Iphone",
@@ -15,7 +12,7 @@ export default class App extends Component {
         btn1: "Add To Cart",
         btn2: "Buy Now",
       },
-  
+
       {
         id: 2,
         name: "Buds",
@@ -23,53 +20,97 @@ export default class App extends Component {
         btn1: "Add To Cart",
         btn2: "Buy Now",
       },
+
       {
         id: 3,
-        name: "Dry Fruit",
-        img: "/image/f2.webp",
-        btn1: "Add To Cart",
-        btn2: "Buy Now",
-      },
-      {
-        id: 4,
-        name: "Samsung",
-        img: "/image/jam.webp",
+        name: "Tablet",
+        img: "/image/a1.webp",
         btn1: "Add To Cart",
         btn2: "Buy Now",
       },
     ];
 
+    const Other_Products = [
+      {
+        id: 4,
+        name: "Dry Fruit",
+        img: "/image/f2.webp",
+        btn1: "Add To Cart",
+        btn2: "Buy Now",
+      },
+
+      {
+        id: 5,
+        name: "Jam",
+        img: "/image/jam.webp",
+        btn1: "Add To Cart",
+        btn2: "Buy Now",
+      },
+      {
+        id: 6,
+        name: "Dry Fruit",
+        img: "/image/f2.webp",
+        btn1: "Add To Cart",
+        btn2: "Buy Now",
+      },
+    ];
     return (
       <>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className=" text-center p-2 mb-2 fw-bold  text-warning">Ecommerse App</h1>
+            </div>
+{/* ********************************************** */}
+     <div className="col-md-12 text-center bg-dark p-3" >
+     <button className="btn btn-secondary px-4 fw-bold my-3">All Product</button>
+      <button className="btn btn-danger px-4 mx-4 fw-bold my-3">Electronic Product</button>
+      <button className="btn btn-primary px-4 fw-bold my-3">Other Product</button>
+     </div>
 
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-12">
-            <h1 className=' text-center p-4 my-3 fw-bold  text-warning'>Ecommerse App</h1>
-          </div>
+{/* ********************************************** */}
 
             <div className="col-md-12">
               <div className="row">
+                {Elect_Prod.map((val) => {
+                  return (
+                    <div className="col-md-3" key={val.id}>
+                      <Products
+                        name={val.name}
+                        img={val.img}
+                        btn1={val.btn1}
+                        btn2={val.btn2}
+                      />
+                    </div>
 
-                {
-                  Product_Data.map((val)=>{
-                    return(
-                      
-                      
-                <div className="col-md-3"  key={val.id}>
-                <Products name={val.name}  img={val.img} btn1={val.btn1} btn2={val.btn2}/>
-                </div>
-                      
-                    )
-                  })
-                }
-             
+                    // **************************
+
+                    // other product
+                  );
+                })}
+
+{
+                      Other_Products.map((val)=>{
+                      return(
+                        
+                        <div className="col-md-3" key={val.id}>
+                        <Products
+                        name={val.name}
+                        img={val.img}
+                        btn1={val.btn1}
+                        btn2={val.btn2}
+                      />
+                        </div>
+                        
+                      )
+                    })
+
+                    }
               </div>
             </div>
+          </div>
         </div>
-      </div>
-
       </>
-    )
+    );
   }
 }
--- React_Day_7_Code_Categorywise_Products_App_Updated/myapp/src/main.jsx
+++ React_Day_7_Code_Categorywise_Products_App_Updated/myapp/src/main.jsx
@@ -4,8 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import App2 from './App2.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* <App2/> */}
   </React.StrictMode>,
 )
