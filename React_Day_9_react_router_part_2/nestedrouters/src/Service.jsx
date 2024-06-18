69f7dd6b962ab8b059e8c510c1ea01a798529f22
nikhil kamble <nikhilkamble19985@gmail.com>
2024-06-18 17:48:44 +0530

    nested Routing with navbar and oulet

---
+++ React_Day_9_react_router_part_2/nestedrouters/src/Service.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <>
+      <h1 className="text-center my-4 text-primary">Service COmponent</h1>
+    </>
+  );
+};
+
+export default Service;
